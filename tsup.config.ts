0d8b30e3c897f90bbb4e2ee6d14cc22fbdb422a7
Doruk Özer <dorukozer@protonmail.com>
2026-02-04 12:39:34 +0300

    docs: project ascii header update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,35 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => ({
-  entry: options.watch
-    ? ['src/**/*']
-    : ['src/index.ts', 'src/llm/mcp/server/*'],
+export default defineConfig((_options) => ({
+  name: 'kisuke.vim',
+  entry: ['src/index.ts', 'src/llm/mcp/server/*'],
   format: ['esm'],
-  // splitting: true,
-  // bundle: true,
-  // clean: !options.watch,
-  // treeshake: true,
+  banner: {
+    js: `/**
+*
+*   ██ ▄█▀ ██▓  ██████  █    ██  ██ ▄█▀▓█████
+*    ██▄█▒ ▓██▒▒██    ▒  ██  ▓██▒ ██▄█▒ ▓█   ▀
+*   ▓███▄░ ▒██▒░ ▓██▄   ▓██  ▒██░▓███▄░ ▒███
+*   ▓██ █▄ ░██░  ▒   ██▒▓▓█  ░██░▓██ █▄ ▒▓█  ▄
+*   ▒██▒ █▄░██░▒██████▒▒▒▒█████▓ ▒██▒ █▄░▒████▒
+*   ▒ ▒▒ ▓▒░▓  ▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ▒ ▒▒ ▓▒░░ ▒░ ░
+*   ░ ░▒ ▒░ ▒ ░░ ░▒  ░ ░░░▒░ ░ ░ ░ ░▒ ▒░ ░ ░  ░
+*   ░ ░░ ░  ▒ ░░  ░  ░   ░░░ ░ ░ ░ ░░ ░    ░
+*   ░  ░    ░        ░     ░     ░  ░      ░  ░
+*
+*   At first this project was my sandbox environment for vim script, then
+*   I set some simple and primitive goals while feeling like its beyond my
+*   scope and I can't finish them. Now I'm unemployed again and I'm
+*   trying to turn this into my goth waifu AI girlfriend like my life
+*   depends on it. My only goal is building something that gonna keep me
+*   in this inefficient, meaningless, delusional, poser, tryhard, fake
+*   state I just can't or don't or not prefer to let go.
+*
+**/`
+  },
   outDir: 'dist',
-  // skipNodeModulesBundle: true,
   target: 'esnext',
-  platform: 'node'
+  platform: 'node',
+  clean: true,
+  sourcemap: 'inline'
 }));
